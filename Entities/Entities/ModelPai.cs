ef7fa94d336a090563181c75f44620e6781387db
Gabriel Serafim Souza <gabriel.souza@b2egroup.com.br>
2021-05-30 17:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entities/ModelPai.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class ModelPai
+    {
+        public List<Produto> Carrinho { get; set; }
+        public IList<CompraUsuario> Total { get; set; }
+    }
+}
